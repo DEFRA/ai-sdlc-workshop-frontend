f5c6147b9b708f72c84b1ce5bfa6c2e2f310bea3
Ben Wilkes <ben@eclipseitconsulting.co.uk>
2025-05-08 16:27:47 +0100

    feat: tweaks to base code

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    res.render('pages/home', {
+    res.render('pages/index', {
       pageTitle: 'Home'
     });
   } catch (error) {
